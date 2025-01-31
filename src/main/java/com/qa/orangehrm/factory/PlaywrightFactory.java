d3ed5c1e05990bd24aa05d59dd0120b6b896ea49
Mohamed Saleh <mohamedsaleh181@gmail.com>
2025-01-31 03:38:49 +0200

    add readme file v7

--- src/main/java/com/qa/orangehrm/factory/PlaywrightFactory.java
+++ src/main/java/com/qa/orangehrm/factory/PlaywrightFactory.java
@@ -31,7 +31,7 @@ public class PlaywrightFactory {
                 browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
                 break;
             case "firefox":
-                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true));
                 break;
             case "safari":
                 browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
